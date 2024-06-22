c71f36de5da9e7f4fc786bd4262e6ff7d9350989
kiran-umesh <kiran_40221@yahoo.co.in>
2024-06-22 11:40:10 +0530

    committing post revert

--- firstfileforgit.py
+++ firstfileforgit.py
@@ -1,4 +1,8 @@
 print ("Hello Everyione, am trying out Git")
+<<<<<<< HEAD
 print ("added additonal line")
        print ("added 2nd line)
 print ("added a 2nd line to the code")
+=======
+print ("added a 2nd line to the code")
+>>>>>>> parent of 41bb793 (updated file in test branch)
